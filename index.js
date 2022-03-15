eb3595fda84056b1447205bdf07b3bbc4e473b88
Ramki Marichamy <ramkipandian@ramkis-MacBook-Pro.local>
2022-03-15 16:25:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+const CONNECTION_URL = 'mongodb+srv://ramki45:pandian5!@cluster0.vna1k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+ // mongoose.set('useFindAndModify', true);
+
+//http://localhost:3002/posts
+
