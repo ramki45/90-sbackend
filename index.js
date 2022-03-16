a9563ba481120342ae442f634850bb09a3571c62
Ramki <89958455+ramki45@users.noreply.github.com>
2022-03-16 18:53:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://ramki45:pandian5!@cluster0.vna1k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = 80;
+const PORT = 5000;
 
 mongoose.connect(CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
