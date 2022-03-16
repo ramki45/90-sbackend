14780cc0deda3441ba4b77eb34519a5d07788212
Ramki Marichamy <ramkipandian@ramkis-MacBook-Pro.local>
2022-03-16 18:43:19 +0530

    second commit

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://ramki45:pandian5!@cluster0.vna1k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = 80;
 
 mongoose.connect(CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
- // mongoose.set('useFindAndModify', true);
 
-//http://localhost:3002/posts
+
+
 
